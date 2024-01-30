f9d13c22c5c1be81fd5a558f32dd10c5e115b169
Aaron Armstrong <aaronarmstrong1490@gmail.com>
2024-01-30 15:38:06 +0000

    added css, linked CSS, added JS, linked JS, added pseudocode to js

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+    
+// Load previous search history from local storage (array format)
+
+// Iterate through the array with a loop and display each previous search in the left side under search bar
+
+// Add search bar input function that has API end point for openweathermap
+
+// when searching for a city that has already been searched for previously, it wont be added to the search history
+
+// 5 day weather forecast display on the right side
+
+// on click function for submit button for search
+
+// on click function for each button of previously searched city
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
