2637151a712cdea7436cfc52afe9be5320aef1ec
Aaron Armstrong <aaronarmstrong1490@gmail.com>
2024-03-06 13:16:36 +0000

    fix: created function for displaying search history and saving the local storage, haven't tested yet as I still need to fix the fetch API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,43 @@
 $(document).ready(function () {
+
 // Define variables that I'm going to need
 var APIkey = '3931383ffe79bcfc153c07435437822b';
-var searchBox = $(`#search-input`);
 var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${searchBox}&appid=${APIkey}`;
+var searchBox = $(`#search-input`);
 var submitBtn = $("#search-button");
-var oldSearch = $(`#history`);
-var currentDate = dayjs().format(`DD/MM/YYYY`);
+var historyList = $(`#history`);
+var todaySection = $(`#today`);
+var forecastSection = $(`#forecast`);
+var currentDate = dayjs().format("DD/MM/YYYY");
 
 
 
-// Load previous search history from local storage (array format)
-function previousSearches() {
-
+// load the previous search history from the local storage (in an array format)
+function loadSearchHistory() {
+var searchHistory = JSON.parse(localStorage.getItem(`searchHistory`)) || [];
+// display the search history in the left sidebar
+displaySearchHistory(searchHistory);
 }
 
-
-
 // Iterate through the array and display each previous search in the left side under search bar to a maximum of 6. When we hit 6 we insert the latest search but forget the oldest search
+function displaySearchHistory(searchHistory) {
+  historyList.empty();
+  for (var i = 0; i < Math.min(searchHistory.length, 6); i++) {
+    var listItem = $("<button>").text(searchHistory[i]);
+    historyList.prepend(listItem)
+  }
+}
+
+// save previous search history to local storage
+function saveToLS(search) {
+  var searchHistory = JSON.parse(localStorage.getItem(`searchHistory`)) || [];
+  if (!searchHistory.includes(search)) { // additional check to ensure that the search ISNT a duplicate
+    searchHistory.push(search); // add the newly added search to the history array
+    localStorage.setItem(`searchHistory`, JSON.stringify(searchHistory)); // save the updated history array to local storage
+    displaySearchHistory(searchHistory); // display the updated history
+  }
+}
+
 
 // fetch the queryURL from the API
 fetch(queryURL)
@@ -29,14 +50,6 @@ fetch(queryURL)
   });
 
 
-
-// Save search history to local storage
-// function runSearch(event,) {
-    
-// }
-
-// when searching for a city that has already been searched for previously, it wont be added to the search history
-
 // 5 day weather forecast display on the right side when a city is searched
 
 // click event that take the value of user input in searchBox and declares it as the variable called search
@@ -46,7 +59,7 @@ submitBtn.onclick(function (event) {
 
 })
 
-// on click function for each button of previously searched city to go back and look
+
 
 
 
